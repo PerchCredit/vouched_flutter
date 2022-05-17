0e6a0e5ea638d796aec4c58f374f6b0fd4953fb0
sarbagyastha <sarbagyastha@gmail.com>
2022-05-17 07:37:45 +0545

    :zap: switched to `AndroidView`

--- lib/src/vouched_scanner.dart
+++ lib/src/vouched_scanner.dart
@@ -162,28 +162,12 @@ class _PlatformView extends StatelessWidget {
       );
     }
 
-    return PlatformViewLink(
+    return AndroidView(
       viewType: viewType,
-      surfaceFactory: (context, controller) {
-        return AndroidViewSurface(
-          controller: controller as AndroidViewController,
-          gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
-          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-        );
-      },
-      onCreatePlatformView: (params) {
-        Future.microtask(onViewCreated);
-        return PlatformViewsService.initSurfaceAndroidView(
-          id: params.id,
-          viewType: viewType,
-          layoutDirection: TextDirection.ltr,
-          creationParams: creationParams,
-          creationParamsCodec: const StandardMessageCodec(),
-          onFocus: () => params.onFocusChanged(true),
-        )
-          ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
-          ..create();
-      },
+      layoutDirection: TextDirection.ltr,
+      creationParams: creationParams,
+      creationParamsCodec: const StandardMessageCodec(),
+      onPlatformViewCreated: (_) => Future.microtask(onViewCreated),
     );
   }
 }
